25008122080035b01a3932d4a7d4ae2643a96f98
victory-et <tuduov@gmail.com>
2024-02-28 11:27:41 +0100

    added directives

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+const message = ref("Hello World");
+const userInput = ref("");
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ message }}</h1>
+  <input v-model="userInput" placeholder="what is your name..." />
+  <p>You typed: {{ userInput }}</p>
 </template>
 
 <style scoped>
